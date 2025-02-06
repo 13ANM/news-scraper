08b5217b45afedfe07b2685af9807d4205743e1d
Andrei Mocanu <andreimcornel@gmail.com>
2025-02-06 16:10:34 +0200

    Add `prettier`

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,37 @@
 import { useEffect, useState } from 'react'
 
 interface NewsItem {
-	title: string
-	link: string
-	source: string
+  title: string
+  link: string
+  source: string
 }
 
 const App = () => {
-	const [news, setNews] = useState<NewsItem[]>([])
+  const [news, setNews] = useState<NewsItem[]>([])
 
-	useEffect(() => {
-		fetch('/scraped-data.json')
-			.then((res) => res.json())
-			.then((data) => setNews(data))
-			.catch(console.error)
-	}, [])
+  useEffect(() => {
+    fetch('/scraped-data.json')
+      .then(res => res.json())
+      .then(data => setNews(data))
+      .catch(console.error)
+  }, [])
 
-	return (
-		<div className='p-4'>
-			{news.map((item, idx) => (
-				<div key={idx} className='mb-6'>
-					<a
-						className='text-xl font-bold text-blue-500 underline'
-						href={item.link}
-					>
-						{item.title}
-					</a>
+  return (
+    <div className='p-4'>
+      {news.map((item, idx) => (
+        <div key={idx} className='mb-6'>
+          <a
+            className='text-xl font-bold text-blue-500 underline'
+            href={item.link}
+          >
+            {item.title}
+          </a>
 
-					<p className='text-gray-600'>{item.source}</p>
-				</div>
-			))}
-		</div>
-	)
+          <p className='text-gray-600'>{item.source}</p>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default App
